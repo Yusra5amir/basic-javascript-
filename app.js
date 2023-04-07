cf96faeac326cbcc6406d72f40837af9535208a3
Yusra5amir <125461837+Yusra5amir@users.noreply.github.com>
2023-04-07 20:47:07 +0500

    practice of JS

---
+++ app.js
@@ -0,0 +1,18 @@
+// <!---4 april---->
+alert('Hello');
+prompt('Your Name Please','Yusra')
+var arr=('Hello','Hi')
+ console.log(arr);
+ var arr=('Hello Pakistan');
+ console.log(arr);
+ console.assert(hello);
+ var alert=('main thek hon');
+ console.log("3"+"5");
+ console.log(2+4);
+ var arr=("num")
+
+ var arr= [ 'Hello' , 'Hi']
+document.write(arr)
+console.log(arr)
+ 
+
---
+++ index.js
@@ -0,0 +1,25 @@
+//  <....6 april....>
+ // This is my first javascript code!
+  console.log('Hello everyone');
+  let name='YusraAmir';
+  console.log(name);
+  alert("Hello");
+  prompt('Your Name Please','Yusra');
+  var arr=('Hello','Hi')
+  console.log(arr);
+  
+  // Cannot be a reserved keyword
+  // Should be meaningful
+  // Cannot start with a number (2name)
+  // Cannot cannot a space or hyphen(-)
+  //Are case-sensitive
+
+  let firstname = 'Yusra';
+  let fname = 'Amir'
+
+let name='Yusra';//String Literal
+let age=18;
+let isApproved = false //boolean literal
+let firstName = undefined;
+let lastName = undefined;
+let selectedColor = null;
